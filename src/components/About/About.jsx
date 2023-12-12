d6aa67004d4ca01c8e95cef0c13b5ede4527d426
matthew dadson <matthewdadson@matthews-MacBook-Pro.local>
2023-12-12 15:38:52 -0600

    initial

---
+++ src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.jpeg")}
+          alt="Me  with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                Web pages.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Data Analyst</h3>
+              <p>
+                I have experience working with large data sets and performing data Analytics to
+                help drive bussines decisions.
+              </p>
+            </div>
+          </li>
+          
+        </ul>
+      </div>
+    </section>
+  );
+};
